fd92cb6447b4b9904742ae9d6b980b9baedaef24
bebopvinh <vinhdo07@gmail.com>
2019-05-28 12:34:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+$(document).ready(function() {
+  console.log("jQuery is ready!")
+})
+
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
@@ -5,3 +9,8 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  const inValue =  $('input[name="name"]')
+  console.log(inValue[0])
+}
