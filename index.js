c5c511f692f6846ded748dfa06e27492dc1843cc
Burak Kocak <burakkocak884@ghotmail.com>
2019-05-30 16:50:30 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,40 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+	// debugger;
+	const em = document.querySelector('form input').value;
+	// debugger;
+	return em;
+}
+function nameClear(){
+	document.querySelector('form input').value = "";
+}
+
+
+function addNewElementAsLi(){
+	const employee = retrieveEmployeeInformation();
+	const li = document.createElement('li');
+	const ul = document.querySelector('ul.employee-list');
+	ul.appendChild(li);
+	li.innerHTML = employee;
+	}
+
+
+function addNewLiOnClick(){
+	// debugger;
+	
+	const submitButton = document.querySelectorAll('form input')[1];
+	submitButton.addEventListener('click',function(e) {addNewElementAsLi(); 
+     nameClear();
+	});
+}
+function clearEmployeeListOnLinkClick(){
+	// debugger;
+ const  clearButton = document.querySelectorAll('a');
+ clearButton[0].addEventListener('click', function(e) { e.preventDefault();
+ 	document.querySelector('ul').innerHTML = '';
+
+ });
+}
