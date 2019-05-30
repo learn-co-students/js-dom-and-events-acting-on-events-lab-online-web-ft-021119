ce5a1f380edd962754d583ce6a4bcf797246e9e4
joanrig <github email address>
2019-05-29 21:35:58 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+const list = document.querySelector('ul.employee-list')
+
+function retrieveEmployeeInformation(){
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi(){
+  let newLi = document.createElement('li')
+  newLi.innerHTML = retrieveEmployeeInformation()
+  list.appendChild(newLi)
+}
+
+function addNewLiOnClick(){
+  let name = document.querySelector('form input[name="name"]')
+  let button = document.querySelector('form input[type="submit"]')
+
+  button.addEventListener('click', function(event) {
+    addNewElementAsLi()
+  name.value = ""
+  })
+}
+
+function clearEmployeeListOnLinkClick(){
+  debugger
+  let link = document.querySelector('a')
+  link.addEventListener('click', function(event) {
+    list.removeChild(list.querySelectorAll('li'))
+  })
+}
