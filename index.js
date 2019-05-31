4db2fba1a298dad958ac094597f0fe5313eba4a4
bbennett7 <bryn.bennett@gmail.com>
2019-05-31 10:49:52 -0700

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let list = document.querySelector('ul.employee-list')
+
+  let listItem = document.createElement('li');
+  listItem.innerHTML = retrieveEmployeeInformation();
+
+  list.appendChild(listItem);
+}
+
+function addNewLiOnClick() {
+  document.querySelector('form').addEventListener('click', addNewElementAsLi());
+
+  document.querySelector('input').value = null;
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function() {
+    let list = document.querySelector('ul.employee-list');
+
+    while(!!list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
+  });
+}
