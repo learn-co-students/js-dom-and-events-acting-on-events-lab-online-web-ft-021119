4722e14b6f18b5baae71772fcb3caff97a94e10b
kirstenwerner <kirstenmwerner@gmail.com>
2019-06-03 18:53:44 -0700

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+const employeeList = document.querySelector('ul.employee-list')
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  const newLi = document.createElement('li')
+  newLi.innerHTML = retrieveEmployeeInformation()
+  employeeList.appendChild(newLi)
+}
+
+function addNewLiOnClick() {
+  const nameField = document.querySelector('input[name="name"]')
+  const button = document.querySelector('input[type="submit"]')
+  button.addEventListener('click', addNewElementAsLi())
+  nameField.value = ""
+}
+
+function clearEmployeeListOnLinkClick() {
+  const clearLink = document.querySelector('a')
+  clearLink.addEventListener('click', () => {
+    while(!!employeeList.firstChild) {
+      employeeList.removeChild(employeeList.firstChild)
+    }
+  })
+}
