4a4c5dc9fa85b4bb31dbec28d4e04a4a8d0e2a95
VBTX <jv.svedaite@gmail.com>
2019-06-04 16:13:10 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,38 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+
+function retrieveEmployeeInformation(){
+	const input = document.querySelector('input');
+	return input.value;
+
+}
+
+function addNewElementAsLi(){
+	const newEl = retrieveEmployeeInformation();
+	let element = document.createElement('li');
+	element.innerHTML = newEl;
+	document.querySelector('.employee-list').appendChild(element);
+}
+
+function addNewLiOnClick(){
+	const eventful = document.querySelector('input[type=submit]');
+	eventful.addEventListener('click', function(event){
+		addNewElementAsLi()
+		document.querySelector('input').value = ''
+	})
+
+}
+
+function clearEmployeeListOnLinkClick(){
+	const buttonEm = document.querySelector('a');
+	buttonEm.addEventListener('click', function(event){
+		let listEm = document.querySelector('ul')
+		listEm.innerHTML = '';
+	}) 
+
+}
+
+
+// clearEmployeeListOnLinkClick(): This empties out the employee list when "Clear employee list" button clicked.
